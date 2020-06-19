9916a3c28a662d302ec310d470d9d7a4b0fd453a
Andrea Zanti <andrea.zanti@outlook.com>
2020-06-19 17:57:57 +0200

    *

--- users/auth/backends.py
+++ users/auth/backends.py
@@ -12,7 +12,7 @@ class AuthenticationBackend(ModelBackend):
         except UserModel.DoesNotExist:
             # Run the default password hasher once to reduce the timing
             # difference between an existing and a nonexistent user (#20760).
-            # UserModel().set_password(password)
+            UserModel().set_password(password)
             pass
         else:
             if user.check_password(password) and self.user_can_authenticate(user):
