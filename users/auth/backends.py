f99a6f0eb376e29e00442b86f58837cc242910cb
Andrea Zanti <andrea.zanti@outlook.com>
2020-06-22 18:02:49 +0200

    fix

--- users/auth/backends.py
+++ users/auth/backends.py
@@ -9,10 +9,10 @@ class AuthenticationBackend(ModelBackend):
             return
         try:
             user = User._default_manager.get_by_natural_key(username)
-        except UserModel.DoesNotExist:
+        except User.DoesNotExist:
             # Run the default password hasher once to reduce the timing
             # difference between an existing and a nonexistent user (#20760).
-            UserModel().set_password(password)
+            User().set_password(password)
             pass
         else:
             if user.check_password(password) and self.user_can_authenticate(user):
